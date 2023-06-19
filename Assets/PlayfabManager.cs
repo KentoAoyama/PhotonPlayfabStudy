1d0a588b804c23596ce8ee4c6d36a6d66ec4e9af
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-06-19 16:02:38 +0900

    [add]FirstCommit

---
+++ Assets/PlayfabManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    void Start()
+    {
+        Login();
+    }
+
+    private void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log($"LoginSuccess!!  {result}");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.Log($"LoginFailure!!  {error.GenerateErrorReport()}");
+    }
+}
